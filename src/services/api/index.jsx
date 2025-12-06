6306c6c4ca1d14e22bb2e6071d65cd6e36d55a04
Avinash Shrivastav <avinash.shrivastav@revv.co.in>
2025-12-06 13:10:05 +0530

    c

--- src/services/api/index.jsx
+++ src/services/api/index.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/v1",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:3000/v1" ||
+    "https://web-developement-backend.vercel.app/v1",
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
